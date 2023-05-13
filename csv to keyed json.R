847a30f2f386e745704196a0e24a186593c0c6c3
adityendrasuman <34791829+adityendrasuman@users.noreply.github.com>
2023-05-13 22:04:49 +0800

    Add files via upload

--- csv to keyed json.R
+++ csv to keyed json.R
@@ -48,6 +48,38 @@ y2 = paste0('{"C":[',cou,']}')
 nchar(y2)
 writeLines(y2, "pincode_latest_json_long.txt")
 
+#=======================
+
+c_s <- x %>% 
+  select (C, S) %>% 
+  unique() %>% 
+  group_by(C) %>% 
+  summarise(S = paste(S,collapse = '","')) %>% 
+  mutate(S = paste0('{"', S, '"}')) %>% 
+  mutate(Type = "Country") %>% 
+  select(Type, X = C, Y = S)
+
+s_d <- x %>% 
+  select (S, D) %>% 
+  unique() %>% 
+  group_by(S) %>% 
+  summarise(D = paste(D,collapse = '","')) %>% 
+  mutate(D = paste0('{"', D, '"}')) %>% 
+  mutate(Type = "State") %>% 
+  select(Type, X = S, Y = D)
+
+d_p <- x %>% 
+  select (D, P) %>% 
+  unique() %>% 
+  group_by(D) %>% 
+  summarise(P = paste(P,collapse = ',')) %>% 
+  mutate(P = paste0('{', P, '}')) %>% 
+  mutate(Type = "District") %>% 
+  select(Type, X = D, Y = P)
+
+y3 <- rbind(c_s, s_d, d_p) %>% 
+  write.csv("pincode_latest_map.csv", row.names = F)
+
 #---------------------  
 # {
 #   "C":[
@@ -76,3 +108,8 @@ writeLines(y2, "pincode_latest_json_long.txt")
 #         
 #    ]
 # }
+
+#-------------------------------------
+"INDIA": ["S1": {"D1":{P1, P2, P3}, "D2":{P4, P5, P6}, ...}, "S2": {"D1":{P1, P2, P3}, "D2":{P4, P5, P6}, ...}]
+
+
